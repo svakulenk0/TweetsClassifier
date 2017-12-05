02e906af5bbf7bdc2becd1f7c4a494b3b6f2d8e2
vendi <svitlana.vakulenko@gmail.com>
2017-12-05 17:04:16 +0100

    new: script for running Tweet2Vec training

---
+++ run.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+svakulenko
+5 December 2017
+
+Load tweets from MongoDB and train Tweet2Vec NN model
+'''
+from load_from_mongo import load_data_from_mongo
+from train import split_dataset
+
+
+X, y = load_data_from_mongo("communityTweets", "cs_conferences",
+                            x_field="clean_text", y_field="label", limit=20)
+(X_train, y_train), (X_validate, y_validate), (X_test, y_test) = split_dataset(X, y)
+train(train_data, val_data, save_path="./model/")
