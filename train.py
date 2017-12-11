8861a12427deeffd01f27f10497075ba66d13b26
vendi <svitlana.vakulenko@gmail.com>
2017-12-11 20:31:55 +0100

    fix: stick to the provided classes

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ def build_dictionary(text):
     freqs = charcount.values()
     sorted_idx = np.argsort(freqs)[::-1]
 
-    chardict = Counter()
+    chardict = OrderedDict()
     for idx, sidx in enumerate(sorted_idx):
         chardict[chars[sidx]] = idx + 1
 
@@ -54,34 +54,29 @@ def build_label_dictionary(labels):
     freqs = labelcount.values()
     sorted_idx = np.argsort(freqs)[::-1]
 
-    labeldict = Counter()
+    labeldict = OrderedDict()
     for idx, sidx in enumerate(sorted_idx):
-        labeldict[labels[sidx]] = idx + 1
+        labeldict[labels[sidx]] = idx
+
     return labeldict, labelcount
 
 
-def save_dictionary(worddict, wordcount, loc):
+def save_dictionary(_dict, count, loc):
     '''
     Save a dictionary into the specified location 
     '''
     with open(loc, 'w') as f:
-        pkl.dump(worddict, f)
-        pkl.dump(wordcount, f)
+        pkl.dump(_dict, f)
+        pkl.dump(count, f)
 
 
 class BatchTweets():
 
-    def __init__(self, data, targets, labeldict, batch_size=128, max_classes=1000, test=False):
+    def __init__(self, data, targets, labeldict, batch_size=128):
         # convert targets to indices
-        if not test:
-            tags = []
-            for l in targets:
-                tags.append(labeldict[l] if l in labeldict and labeldict[l] < max_classes else 0)
-        else:
-            tags = []
-            for line in targets:
-                tags.append([labeldict[l] if l in labeldict and labeldict[l] < max_classes else 0 for l in line])
-
+        tags = []
+        for l in targets:
+            tags.append(labeldict[l])
         self.batch_size = batch_size
         self.data = data
         self.targets = tags
@@ -123,7 +118,7 @@ class BatchTweets():
 
 def schedule(lr):
     print("Updating learning rate...")
-    lr = max(1e-5, lr/2)
+    lr = max(1e-5, lr / 2)
     return lr
 
 
@@ -168,7 +163,7 @@ def classify(tweet, t_mask, params, n_classes, n_chars):
     return lasagne.layers.get_output(l_dense), l_dense, lasagne.layers.get_output(emb_layer)
 
 
-def train(Xt, yt, Xv, yv, save_path,
+def train_model(Xt, yt, Xv, yv, save_path,
           num_epochs=NUM_EPOCHS, lr=LEARNING_RATE, mu=MOMENTUM,
           reg=REGULARIZATION, sch=SCHEDULE):
     '''
@@ -199,8 +194,8 @@ def train(Xt, yt, Xv, yv, save_path,
     params['b_cl'] = theano.shared(np.zeros((n_classes)).astype('float32'), name='b_cl')
 
     # iterators
-    train_iter = BatchTweets(Xt, yt, labeldict, batch_size=N_BATCH, max_classes=n_classes)
-    val_iter = BatchTweets(Xv, yv, labeldict, batch_size=N_BATCH, max_classes=n_classes, test=True)
+    train_iter = BatchTweets(Xt, yt, labeldict, batch_size=N_BATCH)
+    val_iter = BatchTweets(Xv, yv, labeldict, batch_size=N_BATCH)
 
     print("Building...")
     
@@ -250,7 +245,7 @@ def train(Xt, yt, Xv, yv, save_path,
                 lr = schedule(lr)
                 updates = lasagne.updates.nesterov_momentum(cost, lasagne.layers.get_all_params(net), lr, momentum=mu)
                 train = theano.function(inps, cost, updates=updates)
-                T1 = T1/2
+                T1 = T1 / 2
 
         # stopping criterion: runs for minimum 7 epochs
         if len(valcosts) > 6:
@@ -261,7 +256,7 @@ def train(Xt, yt, Xv, yv, save_path,
                 break
 
         ud_start = time.time()
-        for xr,y in train_iter:
+        for xr, y in train_iter:
             n_samples +=len(xr)
             uidx += 1
             x, x_m = prepare_data(xr, chardict, n_chars=n_char)
@@ -270,7 +265,7 @@ def train(Xt, yt, Xv, yv, save_path,
                 uidx -= 1
                 continue
 
-        curr_cost = train(x,x_m,y)
+        curr_cost = train(x, x_m, y)
         train_cost += curr_cost * len(xr)
         ud = time.time() - ud_start
 
@@ -279,15 +274,15 @@ def train(Xt, yt, Xv, yv, save_path,
             return
 
         if np.mod(uidx, DISPF) == 0:
-            print("Epoch {} Update {} Cost {} Time {}".format(epoch,uidx,curr_cost,ud))
+            print("Epoch {} Update {} Cost {} Time {}".format(epoch, uidx, curr_cost, ud))
 
         if np.mod(uidx,SAVEF) == 0:
             print("Saving...")
         
         saveparams = OrderedDict()
-        for kk,vv in params.iteritems():
+        for kk, vv in params.iteritems():
             saveparams[kk] = vv.get_value()
-            np.savez('%s/model.npz' % save_path,**saveparams)
+            np.savez('%s/model.npz' % save_path, **saveparams)
         
         print("Done.")
 
@@ -302,12 +297,13 @@ def train(Xt, yt, Xv, yv, save_path,
                 print("Validation: Minibatch with zero samples under maxlength.")
                 continue
 
-            vp = predict(x,x_m)
+            vp = predict(x, x_m)
             ranks = np.argsort(vp)[:,::-1]
-            for idx,item in enumerate(xr):
+            for idx, item in enumerate(xr):
                 preds.append(ranks[idx,:])
                 targs.append(y[idx])
 
+            # compute precision @1
             validation_cost = precision(np.asarray(preds), targs, 1)
             regularization_cost = reg_val()
 
@@ -316,7 +312,7 @@ def train(Xt, yt, Xv, yv, save_path,
                 saveparams = OrderedDict()
                 for kk,vv in params.iteritems():
                     saveparams[kk] = vv.get_value()
-                np.savez('%s/best_model.npz' % (save_path),**saveparams)
+                np.savez('%s/best_model.npz' % (save_path), **saveparams)
 
         print("Epoch {} Training Cost {} Validation Precision {} Regularization Cost {} Max Precision {}".format(epoch, train_cost/n_samples, validation_cost, regularization_cost, maxp))
         print("Seen {} samples.".format(n_samples))
@@ -348,7 +344,7 @@ def test_split_dataset():
     assert True in y_test and False in y_test
 
 
-def test_train():
+def test_train_model():
     '''
     Generate sample dataset for binary classification test and split it into 3 sets
     '''
@@ -359,9 +355,9 @@ def test_train():
     X.extend(["or no?"] * N_BATCH)
     y.extend(["no"]  * N_BATCH)
     (X_train, y_train), (X_validate, y_validate), (X_test, y_test) = split_dataset(X, y)
-    train(X_train, y_train, X_validate, y_validate, save_path=MODEL_PATH)
+    train_model(X_train, y_train, X_validate, y_validate, save_path=MODEL_PATH)
 
 
 if __name__ == '__main__':
     # test_split_dataset()
-    test_train()
+    test_train_model()
