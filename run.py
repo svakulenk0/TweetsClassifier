b304b6de3e15fa529e6236383785f5ef32e4fd2d
vendi <svitlana.vakulenko@gmail.com>
2017-12-20 23:35:10 +0100

    new: function for training CS tweets recommender

--- run.py
+++ run.py
@@ -42,6 +42,34 @@ def test_get_random_tweets():
     print y[0]
 
 
+def train_CS_news_recommender():
+    # load CS tweets
+    X1, y1 = load_data_from_mongo_balanced("communityTweets", "cs_conferences",
+                                         x_field="clean_text", y_value="CS", limit=30000)
+    assert X1
+    assert y1
+    print len(X1), 'CS samples loaded'
+
+    # load random tweets
+    X2, y2 = load_data_from_mongo_balanced("tweets", "sample_04_12_2017",
+                                         x_field="clean_text", y_value="random", limit=30000)
+    assert X2
+    assert y2
+    print len(X2), 'random samples loaded'
+
+    # merge data
+    X = X1 + X2
+    y = y1 + y2
+    print len(X), 'samples total'
+
+    # split data
+    assert len(X) == len(y)
+    (X_train, y_train), (X_validate, y_validate), (X_test, y_test) = split_dataset(X, y)
+    
+    # run Tweet2Vec NN
+    train_model(X_train, y_train, X_validate, y_validate)
+    test_model(X_test, y_test)
+
+
 if __name__ == '__main__':
-    test_get_cs_tweets()
-    test_get_random_tweets()
+    train_CS_news_recommender()
