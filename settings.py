ca1116c61d948df4b681f01598ab91330e918085
vendi <svitlana.vakulenko@gmail.com>
2017-12-27 13:38:18 +0100

    chg: increase char embeddings dimensionality

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ based on Tweet2Vec implementation by bdhingra https://github.com/bdhingra/tweet2
 '''
 
 # network size
-CHAR_DIM = 150  # dimensionality of the character embeddings lookup
+CHAR_DIM = 500  # dimensionality of the character embeddings lookup
 MAX_CHAR = 128  # number of unique characters 128 US-ASCII + 1,920 UTF-8
 HDIM = 500  # size of the hidden layer
 LDIM = 2  # number of the unique output labels (categories)
