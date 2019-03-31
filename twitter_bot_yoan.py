83fbfc73b3d0fdc7e714e63e22135ce980c524f6
vendi <svitlana.vakulenko@gmail.com>
2019-04-01 00:10:49 +0200

    fix: remove unused imports

--- twitter_bot_yoan.py
+++ twitter_bot_yoan.py
@@ -37,7 +37,9 @@ class TweetClassifier(StreamListener):
         # Load model and dictionaries
         print("Loading pre-trained model...")
         self.tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
-        self.model = pickle.load(open('%s/%s' % (model_path, model_name), 'rb'))
+        # load the model from disk
+        filename = 'x1.sav'
+        self.mdel = pickle.load(open(filename, 'rb'))
 
     def on_status(self, status):
         # ignore retweets
